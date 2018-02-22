071efc3f9e85c17d068de848cea7102153ff17cc
Ray Villalobos <ray@planetoftheweb.com>
2018-02-22 18:07:25 -0500

    Added jQuery Date Plugin and DOM removal

--- app/js/main.js
+++ app/js/main.js
@@ -2,20 +2,32 @@ $( document ).ready(function() {
 
   var data;
 
-  function listAppointments(info) {
-    $("#petList").loadTemplate($('#template'), info);
+  function removeApt(aptID) {
   }
 
+  function listAppointments(info) {
+    $('#petList').loadTemplate('appointment-list.html', info, {
+      complete: function() {
+        $( '.pet-delete' ).on( 'click', function() {
+          var whichItem = $(this).attr('id');
+          $(this).parents('.pet-item').hide(300, function() {
+            console.log(whichItem);
+            removeApt( whichItem );
+            $(this).remove();
+            });
+        });
+      }
+    });
+  }
 
   $.ajax({
     url: "js/data.json",
     context: document.body,
     dataType: 'JSON'
   }).done(function(data) {
-    console.log(data);
     listAppointments(data);
   });
-  
+
 
 });
 
