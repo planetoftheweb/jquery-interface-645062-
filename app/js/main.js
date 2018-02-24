ad5859f5fa874f4c16dcec24f8baf6aa7e9a146d
Ray Villalobos <ray@planetoftheweb.com>
2018-02-23 19:32:31 -0500

    search and sorting complete

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,26 @@
 $( document ).ready(function() {
 
-  var aptData;
+  var aptData, displayData, sortBy, sortDir;
+
+  sortBy = 'petName';
+  sortDir = 'asc';
 
   function removeApt(aptID) {
     var whichApt = _.find(aptData, function(item){
       return item.id === aptID; 
     });
-
     aptData = _.without(aptData, whichApt);
+    displayData = aptData;
+    console.log(aptData);
   }
 
   function listAppointments(info) {
+    if (sortDir === 'asc') {
+      info = _.sortBy(info, sortBy);
+    } else  {
+      info = _.sortBy(info, sortBy).reverse();
+    }
+
     $('#petList').loadTemplate('appointment-list.html', info, {
       complete: function() {
         $( '.pet-delete' ).on( 'click', function() {
@@ -29,10 +39,46 @@ $( document ).ready(function() {
     context: document.body,
     dataType: 'JSON'
   }).done(function(data) {
-    aptData = data;
-    listAppointments(data);
+    aptData = displayData = data;
+    listAppointments(displayData);
   });
 
+  // EVENTS
+
+  $('.apt-addheading').on( 'click', function() {
+    $('.card-body').toggle(300);
+  });
+
+  $('.sort-by .dropdown-item').on( 'click', function() {
+    var sortDropdown = $(this).attr('id');
+
+    switch (sortDropdown) {
+      case 'petName':
+      case 'ownerName':
+      case 'aptDate':
+        sortBy = sortDropdown;
+        $('.sort-by').removeClass('active');
+        break;
+      case 'asc':
+      case 'desc':
+      $('.sort-dir').removeClass('active');
+      sortDir = sortDropdown;
+        break;
+    }
+    $(this).addClass('active');
+
+    listAppointments(displayData);
+  });
+
+  $('#SearchApts').keyup(function() {
+    var searchText = $(this).val();
+    displayData = _.filter(aptData, function(item){ 
+      return (item.petName.toLowerCase().match(searchText.toLowerCase())) ||
+      (item.ownerName.toLowerCase().match(searchText.toLowerCase())) ||
+      (item.aptNotes.toLowerCase().match(searchText.toLowerCase()))
+    });
+    listAppointments(displayData);
+  });
 
 });
 
