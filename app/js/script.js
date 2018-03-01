77b9bdc36942b5cc6d481b18ec6aaa474b39e137
Ray Villalobos <ray@planetoftheweb.com>
2018-03-01 12:22:17 -0800

    finished Loading data

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,19 @@
 $(function() {
+
+
+  //READ DATA
+  $.ajax({
+    url: 'js/data.json'
+  }).done(function(data) {
+    $('#field-petName').text(data[0].petName);
+    $('#field-ownerName').text(data[0].ownerName);
+    $('#field-aptNotes').text(data[0].aptNotes);
+  });
+
+  //EVENTS
+
   $('.apt-addheading').on('click',function() {
     $('.card-body').toggle(300);
-  });
-});
+  }); // click on add appointment
+
+}); // Document is ready
