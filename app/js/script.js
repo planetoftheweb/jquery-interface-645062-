11bffa1e2271a948bb2338a5c1cb0bb67e2932de
Ray Villalobos <ray@planetoftheweb.com>
2018-03-02 08:59:23 -0800

    02_04 Modifying data

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,29 @@
 $(function() {
 
+  var aptData;
+
+  //FUNCTIONS
+  function removeApt(aptID) {
+    var whichApt = _.find(aptData, function(item) {
+      return item.id == aptID;
+    });
+    aptData = _.without(aptData, whichApt);
+  }
 
   //READ DATA
   $.ajax({
     url: 'js/data.json'
   }).done(function(data) {
+    aptData = data;
 
     $('#petList').loadTemplate('appointment-list.html', data, {
       complete: function() {
         $('.pet-delete').on('click', function() {
           $(this).parents('.pet-item').hide(300, function() {
+            var whichItem = $(this).attr('id');
+            removeApt(whichItem);
+            // data.splice(Number(whichItem), 1);
+            console.log(aptData);
             $(this).remove();  
           });
         }); //delete apt
